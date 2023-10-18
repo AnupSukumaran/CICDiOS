8e04069b87b9a5047bde733f6e4e8f4cd967a737
Sukumar Anup Sukumaran <sukumar.sukumaran@hikvision.com>
2023-10-18 21:06:15 +0100

    Add AppCenter frameworks

--- RetirementCalculator/AppDelegate.swift
+++ RetirementCalculator/AppDelegate.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import AppCenter
+import AppCenterCrashes
+import AppCenterAnalytics
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        AppCenter.start(withAppSecret: "8324ce2d-73df-492f-b33b-d76fe99055c6", services: [Analytics.self, Crashes.self])
         return true
     }
 
